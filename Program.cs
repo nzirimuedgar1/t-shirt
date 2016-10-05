cd5014511325d3e71ab3a1e37a46943f31a1b234
nzirimuedgar1 <edgarnzirimue@yahoo.com>
2016-10-05 09:23:21 -0500

    making commit - pushing new files

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace helloworld
+
+    class Program
+    {
+        static void Main(string 10/03/16[] args)
+        isnt a year
+        keeping the door' close,>
+        }10/03/2016
+i spend 5.45$ on 'cheeseburger'"
+    "x marks the spot
+    }
+}
